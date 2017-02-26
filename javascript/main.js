d96cf790a67cd4953fb813b88aefbb70466d1feb
Rico <rico1310@live.com>
2017-02-26 20:29:51 +0100

    style changes that contains a sticky footer

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,7 @@
 var bannerTop = document.getElementById("banner");
-bannerTop.onmouseover
+bannerTop.onmouseenter = function(){
+    bannerTop.innerHTML = "Click to return to main page"
+};
+bannerTop.onmouseout = function(){
+    bannerTop.innerHTML = "Riccardo's website about building engaging prototypes"
+};
