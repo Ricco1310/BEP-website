c9612b184eed4c1cdbfdd21f4be1f065c723f94e
Rico <rico1310@live.com>
2017-04-02 22:42:03 +0200

    added all week 1 images with caption, fiddled with JS

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,34 @@
 var bannerTop = document.getElementById("banner");
-bannerTop.onmouseenter = function(){
-    bannerTop.innerHTML = "Click to return to main page"
-};
-bannerTop.onmouseout = function(){
-    bannerTop.innerHTML = "Riccardo's website about building engaging prototypes"
-};
+var figures = document.getElementsByTagName("figure");
+var initialDesign = document.getElementById("initialDesign");
+
+function loadImages() {
+    var toggle = false;
+    initialDesign.onmousedown = function () {
+        console.log(55);
+        toggle = !toggle;
+    };
+    if (toggle) {
+        for (var i = 0; i < figures.length; i++) {
+            initialDesign.style.display = "visible";
+        }
+    } else {
+        for (var i = 0; i < figures.length; i++) {
+            initialDesign.style.display = "hidden";
+        }
+    }
+}
+
+function animateBanner() {
+    bannerTop.onmouseenter = function () {
+        bannerTop.innerHTML = "Click to return to main page"
+    };
+    bannerTop.onmouseout = function () {
+        bannerTop.innerHTML = "Riccardo's website about building engaging prototypes"
+    };
+}
+
+window.onload = function () {
+    //loadImages();
+    animateBanner();
+}
